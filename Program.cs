e1ef76268845a41296a64c41b39c79871aaca435
Lucas Vazquez <vazquezlucasmatias2001@gmail.com>
2023-03-02 17:39:21 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ConceptosPOO;
+using System;
+
+namespace ConceptoPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        static void realizarTarea()
+        {
+
+            Punto origen = new Punto();
+
+            Punto destino = new Punto(128, 80);
+
+        }
+    }
+}
---
+++ Punto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    internal class Punto
+    {
+        public Punto(int x, int y)
+        {
+
+            Console.WriteLine($"Coordenada X: {x}, Coordenada Y: {y}");
+
+        }
+
+        public Punto()
+        {
+
+            Console.WriteLine("Este es el contrstructor por defecto");
+
+        }
+    }
+}
